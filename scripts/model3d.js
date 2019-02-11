c1e71812ae964ed20d36691f9763b5390e5a7cf3
diekus <diekus@gmail.com>
2019-02-11 20:41:36 +0000

    update

--- scripts/model3d.js
+++ scripts/model3d.js
@@ -2,86 +2,122 @@ class Model3d extends HTMLElement{
     constructor(){
         super();
 
-        // attaches shadow dom
-        let shadow = this.attachShadow({mode:'open'});
-        
-        //adds babylon
-        let bjs = document.createElement('script');
-        bjs.src = 'https://cdn.babylonjs.com/babylon.js';
-        bjs.async = false;
-        document.head.appendChild(bjs);
-        let bjsloader = document.createElement('script');
-        bjsloader.src = 'https://preview.babylonjs.com/loaders/babylonjs.loaders.min.js';
-        bjsloader.async = false;
-        document.head.appendChild(bjsloader);
-        let pep = document.createElement('script');
-        pep.src = 'https://code.jquery.com/pep/0.4.3/pep.js';
-        pep.async = false;
-        document.head.appendChild(pep);
-        
+        let shadow = this.attachShadow({mode:'open'}); 
         //adds canvas to the component
         let cnv = document.createElement('canvas');
         cnv.setAttribute('id', 'renderCanvas');
         cnv.setAttribute('touch-action', 'none');
+        cnv.style = "width: 100%; height: 100%; touch-action: none;";
         shadow.appendChild(cnv);
 
-        document.addEventListener('DOMContentLoaded', function() {
-            window.setTimeout(function(){const engine = new BABYLON.Engine(cnv, true);}, 2000);
+        let scene = null;
 
-        });
+        //sets up the babylon environment for loading object into it
+        function setUp3DEnvironment(){
+            const engine = new BABYLON.Engine(cnv, true);
 
-        
-
-
-        const model_shell = `
-
-        <style>
-            #renderCanvas {
-                width: 300px;
-                height: 300px;
-                touch-action: none;
-                background-color:yellow;
-            }
-        </style>
-        
-        <script>
-        //
-            let sroot = this.shadowRoot; 
-            const canvas = sroot.querySelector("#renderCanvas"); // Get the canvas element
-            const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
-            
-            const createScene = function () {
+            let createScene = function () {
                 var scene = new BABYLON.Scene(engine);
                 //scene.clearColor = new BABYLON.Color3(1, 1, 1);
                 scene.createDefaultCameraOrLight(true, true, true);
                 return scene;
             }
-            const scene = createScene(); //Call the createScene function
-            engine.runRenderLoop(function () { //starts the render loop
+
+            scene = createScene();
+            //starts the render loop
+            engine.runRenderLoop(function () { 
                 scene.render();
             });
-            window.addEventListener("resize", function () { //manages resizing of browser
+            //manages resizing of container
+            window.addEventListener("resize", function () {
                 engine.resize();
             });
-            
-            // SCENE CREATED
-            
-            let loadGLTF = function(scn, folder, file){
-                var assetsManager = new BABYLON.AssetsManager(scn);
-                let meshTask = assetsManager.addMeshTask("glb task", "", folder, file);
-                meshTask.onSuccess = function (task){
-                    task.loadedMeshes[0].position = BABYLON.Vector3.Zero();
-                }
-                meshTask.onError = function(task, message, exception){
-                    console.log(message, exception);
-                }
-                assetsManager.load();
-            };
+        }
 
-            loadGLTF(scene, '/models/', 'origami-frog.glb')
-        </script>
-        `;
-       
+        //adds babylon to the page
+        (function() {
+            let bjs = document.createElement('script');
+            bjs.src = 'https://cdn.babylonjs.com/babylon.js';
+            bjs.async = false;
+            document.head.appendChild(bjs);
+            let bjsloader = document.createElement('script');
+            bjsloader.src = 'https://preview.babylonjs.com/loaders/babylonjs.loaders.min.js';
+            bjsloader.async = false;
+            document.head.appendChild(bjsloader);
+            let pep = document.createElement('script');
+            pep.src = 'https://code.jquery.com/pep/0.4.3/pep.js';
+            pep.async = false;
+            pep.addEventListener('load', setUp3DEnvironment);
+            document.head.appendChild(pep);
+        })();
+
+        this.getScene = function(){
+            return scene;
+        };
+
+        /*LOAD 3D MODEL*/
+        //method that loads a 3d model into the created scene
+        this.loadGLTF = function(file){
+
+            scene.meshes.pop();
+
+            let path = decodePath(file);
+
+            var assetsManager = new BABYLON.AssetsManager(scene);
+            let meshTask = assetsManager.addMeshTask('glb task', '', path[0], path[1]);
+            meshTask.onSuccess = function (task){
+                task.loadedMeshes[0].position = BABYLON.Vector3.Zero();
+            }
+            meshTask.onError = function(task, message, exception){
+                console.log(message, exception);
+            }
+            assetsManager.load();
+        };
+
+        //separates path from file name in given resource
+        let decodePath = function(path){
+            let fileStart = path.lastIndexOf('/') + 1;
+            let fileName = path.substring(fileStart);
+            let filePath = path.substring(0, fileStart);
+            return [filePath, fileName];
+        };
+    }
+
+    /*HANDLING ATTRIBUTES*/
+    static get observedAttributes(){
+        return ['model-url', 'background-color'];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        switch (name) {
+            case 'model-url':
+                console.log(`loading ${newValue}...` );    
+                this.loadGLTF(newValue); 
+                break;
+            case 'background-color':
+                let s = getScene();
+                s.cl
+                break;            
+            default:
+                break;
+        }
+    }
+
+    get modelUrl(){
+        return this.getAttribute('model-url');
+    }
+
+    get backgroundColor(){
+        return this.getAttribute('background-color');
+    }
+
+    set modelUrl(newValue){
+        this.setAttribute('model-url', newValue);
+    }
+
+    set backgroundColor(newValue){
+        this.setAttribute('background-color', newValue);
     }
 }
+
 customElements.define('model-3d', Model3d);
